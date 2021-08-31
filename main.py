48a92992b8651cdab3d104b83223dc47d3b1cb38
Brian S <88114504+BrianS3-Git@users.noreply.github.com>
2021-08-31 22:04:32 +0000

    Added alternate solution and commented it all out

--- main.py
+++ main.py
@@ -4,8 +4,21 @@ two_digit_number = input("Type a two digit number: ")
 
 ####################################
 #Write your code below this line 👇
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
 
+print("The sum of the two numbers you entered is: " + str(first_digit + second_digit))
 
+##################
+# Could also do this way:
+#two_digit_number = input("Type a two digit number: ")
+#first_digit = (two_digit_number[0])
+#second_digit = (two_digit_number[1])
+
+#result = int(first_digit) + int(second_digit)
+#print(result)
+
+#update
 
 
 
